fba6450f2e335372ab5be7efb93b919e1dce371c
Dyani <dyani70@gmail.com>
2024-01-07 22:41:55 +0900

    Initial Commit

---
+++ WarmUP/WarmUPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmUPApp.swift
+//  WarmUP
+//
+//  Created by Dyani on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WarmUPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
