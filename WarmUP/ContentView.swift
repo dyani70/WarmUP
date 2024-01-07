203e5f9eeed3f0a2ce8348cb9f70ec35eb972fa8
Dyani <dyani70@gmail.com>
2024-01-07 23:13:39 +0900

    Modifier

--- WarmUP/ContentView.swift
+++ WarmUP/ContentView.swift
@@ -10,10 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+                .imageScale(/*@START_MENU_TOKEN@*/.large/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .fontWeight(.bold)
+                .lineLimit(1)
+                .padding(2.0)
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                .accentColor(/*@START_MENU_TOKEN@*/.purple/*@END_MENU_TOKEN@*/)
         }
         .padding()
     }
