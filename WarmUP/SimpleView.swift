b27782502a5128c4fcfd16ae3296b5aa6c361ca2
Dyani <dyani70@gmail.com>
2024-01-09 23:57:51 +0900

    SimpleView

---
+++ WarmUP/SimpleView.swift
@@ -0,0 +1,18 @@
+//
+//  SimpleView.swift
+//  WarmUP
+//
+//  Created by Dyani on 1/9/24.
+//
+
+import SwiftUI
+
+struct SimpleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimpleView()
+}
