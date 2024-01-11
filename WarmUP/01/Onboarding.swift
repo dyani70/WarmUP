f619cb80ea872757d7ecfc808f95734a9c9811fe
Dyani <dyani70@gmail.com>
2024-01-11 23:48:55 +0900

    Onboarding

---
+++ WarmUP/01/Onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding.swift
+//  WarmUP
+//
+//  Created by Dyani on 1/11/24.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding()
+}
