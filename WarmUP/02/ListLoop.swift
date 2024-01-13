666d3f427672cba44d2bf6fed79f4d575ae97bed
Dyani <dyani70@gmail.com>
2024-01-13 23:58:51 +0900

    DataModeling

--- WarmUP/02/ListLoop.swift
+++ WarmUP/02/ListLoop.swift
@@ -7,21 +7,44 @@
 
 import SwiftUI
 
+//Fruit은 반복될 수 있음 Hashable
+struct Fruit: Hashable {
+    let name: String
+    let matchFruitName: String
+    let price: Int
+}
+
 struct ListLoop: View {
     
-    //이곳만 수정하게끔 반복문을 사용한다
-    var fruits = ["Apple", "Banana", "Cherry", "Double Kiwi", "Elder berry"]
+    var favoriteFruits = [
+        Fruit(name: "Apple", matchFruitName: "Banana", price: 1000),
+        
+        Fruit(name: "Banana", matchFruitName: "Banana", price: 3000),
+        
+        Fruit(name: "Cherry", matchFruitName: "Double Kiwi", price: 4000),
+        
+        Fruit(name: "Double Kiwi", matchFruitName: "Elder berry", price: 2400),
+        
+        Fruit(name: "Elder berry", matchFruitName: "Double Kiwi", price: 8000),
+        
+    ]
     
-    var price = ["1000", "3000", "4000", "2400", "8000"]
-    var count = 0
+    //이곳만 수정하게끔 반복문을 사용한다
+    //너무 많은 데이터는 위와 같이 데이터 모델링 시킨다
+//    var fruits = ["Apple", "Banana", "Cherry", "Double Kiwi", "Elder berry"]
+//    var matchFruits = ["Banana", "Banana", "Double Kiwi", "Elder berry", "Double Kiwi"]
+//    
+//    var price = ["1000", "3000", "4000", "2400", "8000"]
+//    var count = 0
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(fruits, id: \.self) { fruit in
-                    HStack {
-                        Text(fruit)
-                        Text(price[count])
+                ForEach(favoriteFruits, id: \.self) { fruit in
+                    VStack(alignment: .leading) {
+                        Text("name: \(fruit.name)")
+                        Text("matchFruitName: \(fruit.matchFruitName)")
+                        Text("price: \(fruit.price)")
                     }
                 }
             }
