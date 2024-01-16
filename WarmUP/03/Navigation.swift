0caf21751c124828406082428d494bf589378788
Dyani <dyani70@gmail.com>
2024-01-17 00:43:29 +0900

    Navigation

---
+++ WarmUP/03/Navigation.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation.swift
+//  WarmUP
+//
+//  Created by Dyani on 1/17/24.
+//
+
+import SwiftUI
+
+struct Navigation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navigation()
+}
