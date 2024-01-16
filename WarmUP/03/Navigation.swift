187f91be16f0ae4bdeee0928764082eec6ea9f90
Dyani <dyani70@gmail.com>
2024-01-17 00:43:57 +0900

    Navigation

--- WarmUP/03/Navigation.swift
+++ WarmUP/03/Navigation.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct Navigation: View {
+    
+    let titles = ["디테일뷰로 이동하기", "디테일뷰로 이동하기2"]
+    let descriptions = ["데스티네이션 입니다", "데스티네이션 입니다2"]
+    
+    @State var showModal: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            
+            List{
+                ForEach([0,1], id: \.self) { index in
+                    NavigationLink {
+                        Text(descriptions[index])
+                    } label: {
+                        Text(titles[index])
+                    }
+                }
+                //반복이니 foreach사용
+//                NavigationLink {
+//                    Text("데스티네이션 입니다")
+//                } label: {
+//                    Text("디테일뷰로 이동하기")
+//                }
+//                
+//                
+//                NavigationLink {
+//                    Text("데스티네이션 입니다2")
+//                } label: {
+//                    Text("디테일뷰로 이동하기2")
+//                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button {
+                        showModal = true
+                    } label: {
+                        Text ("Add")
+                    }
+                }
+            }
+            .sheet(isPresented: $showModal, content: {
+                Text("아이템 추가 페이지 입니다")
+            })
+            .navigationTitle("네비게이션")
+                
+        }
     }
 }
 
